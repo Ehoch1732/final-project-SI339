4f7f39cd3ac72f8e905e1363378034feac4933f6
Erika Hochstein <98134941+Ehoch1732@users.noreply.github.com>
2023-04-01 10:38:20 -0400

    mobile navigation and larger view navigation

--- js/script.js
+++ js/script.js
@@ -9,3 +9,27 @@ function showNav() {
         navList.style.display = "block";
     }
 }; // showNav()
+
+// Target viewports at 640px width & larger
+const tabletQuery = window.matchMedia("(min-width: 640px)");
+
+tabletQuery.onchange = (e) => {
+    console.log("tablet")
+    if (e.matches) {
+        // Display navigation links.
+        let nav = document.getElementById("navlist");
+        nav.style.display = "block";
+    }
+}
+
+// Target mobile viewports at widths below 640px
+const mobileQuery = window.matchMedia("(max-width: 640px)");
+
+mobileQuery.onchange = (e) => {
+    console.log("mobile")
+    if (e.matches) {
+        // Display navigation links.
+        let nav = document.getElementById("navlist");
+        nav.style.display = "none";
+    }
+}
