36e1af495460c7a09fbee85467068e1a34e1fec2
Erika Hochstein <98134941+Ehoch1732@users.noreply.github.com>
2023-04-07 19:05:19 -0400

    check-in #2

--- js/script.js
+++ js/script.js
@@ -33,3 +33,32 @@ mobileQuery.onchange = (e) => {
         nav.style.display = "none";
     }
 }
+
+function currentPage() {
+    let currFile = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+    if (currFile === "index.html") {
+        let currPageInNav = document.getElementById("index");
+        currPageInNav.classList.add("current_page");
+        // Make sure other two pages do not have current_page class.
+        document.getElementById("trips").classList.remove("current_page");
+        document.getElementById("favorite").classList.remove("current_page");
+    }
+    if (currFile === "trips.html") {
+        let currPageInNav = document.getElementById("trips");
+        currPageInNav.classList.add("current_page");
+        // Make sure other two pages do not have current_page class.
+        document.getElementById("index").classList.remove("current_page");
+        document.getElementById("favorite").classList.remove("current_page");
+    }
+    if (currFile === "favorite.html") {
+        let currPageInNav = document.getElementById("favorite");
+        currPageInNav.classList.add("current_page");
+        // Make sure other two pages do not have current_page class.
+        document.getElementById("trips").classList.remove("current_page");
+        document.getElementById("index").classList.remove("current_page");
+    }
+
+
+}
+
+window.addEventListener("load", currentPage);
